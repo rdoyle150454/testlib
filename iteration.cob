0a70eab6b7582b81ac23d518ca99e4c7b089fbb7
RDOYLE <rdoyle>
2014-05-27 07:43:16 +0000

    (iteration.cob;1):new_cobol_project_demo

---
+++ iteration.cob
@@ -0,0 +1,30 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    Iteration.
+       AUTHOR.        rdoyle.
+
+
+       ENVIRONMENT DIVISION.
+	   DATA DIVISION.
+	   WORKING-STORAGE SECTION.
+	   01  Num1           PIC 99  VALUE ZEROS.
+	   01  Num2           PIC 99  VALUE ZEROS.
+	   01  Result         PIC 9999 VALUE ZEROS.
+	   01  Operator       PIC X  VALUE SPACE.
+		
+	   PROCEDURE DIVISION.
+	   MAIN SECTION.
+	       DISPLAY "Enter First Number      : ".
+	       ACCEPT Num1.
+	       DISPLAY "Enter Second Number     : ".
+	       ACCEPT Num2.
+	       DISPLAY "Enter operator (+ or *) : ".
+	       ACCEPT Operator.
+	       IF Operator = "+" THEN
+	        ADD Num1, Num2 GIVING Result
+	       END-IF.
+	       IF Operator = "*" THEN
+	        MULTIPLY Num1 BY Num2 GIVING Result
+	       END-IF.
+	       DISPLAY "Result is of the calculation= ", Result.
+	       STOP RUN.
+	   END PROGRAM Iteration.
