7fb6be1c72e317abe8e399e41fcde28d647878df
RDOYLE <rdoyle>
2014-08-26 09:19:37 +0000

    (project_1.cob;1):new demo file

---
+++ project_1.cob
@@ -0,0 +1,9 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID.     Project_1.
+ENVIRONMENT DIVISION.
+DATA DIVISION.
+PROCEDURE DIVISION.
+Main-program section.
+    DISPLAY "Hello world from the test program!".
+    STOP RUN.
+END PROGRAM Project_1.
